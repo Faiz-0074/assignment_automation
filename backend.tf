14d3ce56164c9e476ff1db897a71da1fa92dbcfd
Faiz Radhanpuri <90353511+Faiz-0074@users.noreply.github.com>
2023-03-19 18:32:40 -0400

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "N01585456-assignment1-RG"
-    storage_account_name = "tfstate5456"
-    container_name       = "tfstate"
-    key                  = "BektUNlQ4kieJ4gGR4Nr+gqcfF/6m0vbQ586x6jWna6ihOG93xd+/+abWpdIJy+8s3DoimBBwEgH+AStSiEwYQ=="
-}
-}
